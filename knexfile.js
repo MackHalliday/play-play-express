68dd7ffd3dcb09137e6bb5e0be84c61f15f3eefb
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-02 15:31:40 -0700

    Add path for production database and seed file
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- knexfile.js
+++ knexfile.js
@@ -23,9 +23,12 @@ module.exports = {
   },
   production: {
     client: 'pg',
-    connection: '',
+    connection: 'postgres://gcfsivxmhkcqhw:618e0eabc514e80ec5d40528cb68dd5879c296ca8dfa9f1c71f48785a7a602ba@ec2-174-129-254-249.compute-1.amazonaws.com:5432/d5e9fb1oevtidm',
     migrations: {
       directory: './db/migrations'
+    },
+    seeds: {
+      directory: './db/seeds/dev'
     },
     useNullAsDefault: true
   }
