8b6f93738e4d26a7c2abb515a5e4a384686f5213
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-02 16:42:24 -0700

    Musix match service working, needs testing
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- services/musix_match_service.js
+++ services/musix_match_service.js
@@ -4,10 +4,9 @@ class MusixMatchService {
   constructor(){
     this.baseUrl = "http://api.musixmatch.com/ws/1.1"
   }
-  async getCoordinatesAsync(songName){
-
+  async getSongData(songName){
     let response = await fetch(`${this.baseUrl}/track.search?q_track=${songName}&apikey=${process.env.MUSIX_MATCH_API_KEY}`)
-    return await response.json()
+    return response.json();
   }
 }
 module.exports = MusixMatchService
