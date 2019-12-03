d788957358f44c43e1ece861fec6d04f9efe1d6b
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-02 17:50:33 -0700

    Remove comments from seed file

--- db/seeds/dev/papers.js
+++ db/seeds/dev/papers.js
@@ -1,20 +1,13 @@
 exports.seed = function(knex) {
-  // We must return a Promise from within our seed function
-  // Without this initial `return` statement, the seed execution
-  // will end before the asynchronous tasks have completed
-  return knex('favorites').del() // delete all favorites
-
-    // Now that we have a clean slate, we can re-insert our paper data
+  return knex('favorites').del()
     .then(() => {
       return Promise.all([
-
-        // Insert a single paper, return the paper ID, insert 2 footnotes
         knex('favorites').insert({
           title: 'Bailamos', artistName: 'Enrique Iglesias', genre: 'Pop', rating: 88
         }, 'id')
         .then(() => console.log('Seeding complete!'))
         .catch(error => console.log(`Error seeding data: ${error}`))
-      ]) // end return Promise.all
+      ])
     })
     .catch(error => console.log(`Error seeding data: ${error}`));
 };
