6fa1beca8192cd595f247250928261e0a8b33cd6
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-02 22:36:11 -0700

    Update song title for Fleedwood Mac song

--- db/seeds/dev/favorites.js
+++ db/seeds/dev/favorites.js
@@ -4,7 +4,7 @@ exports.seed = function(knex) {
       return Promise.all([
         knex('favorites').insert([
           {title: 'Bailamos', artistName: 'Enrique Iglesias', genre: 'Pop', rating: 88},
-          {title: 'Fleetwood Mac', artistName: 'Fleetwood Mac', genre: 'Rock', rating: 52},
+          {title: 'The Chain', artistName: 'Fleetwood Mac', genre: 'Rock', rating: 52},
           {title: 'Truth Hurts', artistName: 'Lizzo', genre: 'Pop', rating: 40},
           {title: 'Baby One More Time', artistName: 'Britney Spears', genre: 'Pop', rating: 91},
           {title: 'Toxic', artistName: 'Britney Spears', genre: 'Pop', rating: 68}
