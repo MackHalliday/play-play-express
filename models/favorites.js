32383429021a9f80831eaf09fed7784ae19de246
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-03 23:42:38 -0700

    Create createfavorites function to sort through response from musix match api and create favorite object with that information
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>

--- models/favorites.js
+++ models/favorites.js
@@ -21,6 +21,21 @@ class Favorites {
   async deleteFavorite(songID) {
     return database('favorites').where('id', songID).del()
   }
+
+  async createFavorite(service) {
+    await database('favorites')
+    .insert({title: service.message.body.track_list[0].track.track_name,
+             artistName: service.message.body.track_list[0].track.artist_name,
+             rating: service.message.body.track_list[0].track.track_rating,
+             genre: service.message.body.track_list[0].track.primary_genres
+                    .music_genre_list[0].music_genre.music_genre_name
+           })
+    // return database('favorites')
+    //   .where('title', service.message.body.track_list[0].track.track_name)
+    //   .columns('id', 'title', 'artistName', 'genre', 'rating');
+
+    return database('favorites').orderBy('created_at', 'desc').first()
+  }
 }
 
 module.exports = Favorites
