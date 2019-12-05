e549f1424b5676149282edebb92bc3182798a32d
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-05 15:03:18 -0700

    Create route for DELETE playlist with error handling

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -33,22 +33,22 @@ router.get('/', async function (request, response) {
 //   }
 // });
 //
-// router.delete('/:id', async function (request, response) {
-//
-//   try {
-//     let favoriteId = await request.params.id
-//     let data = await favorites.findFavorite(favoriteId)
-//     if (data.length != 0){
-//       await favorites.deleteFavorite(data[0].id)
-//       return response.status(204).json(data);
-//     } else {
-//       return response.status(404).json({"error": "Record not found"});
-//     }
-//   }
-//   catch(error) {
-//     return response.status(500).json({"error": "Request could not be handled"});
-//   }
-// });
+router.delete('/:id', async function (request, response) {
+
+  try {
+    let playlistId = await request.params.id
+    let data = await favorites.findPlaylist(playlistId)
+    if (data.length != 0){
+      await playlists.deletePlaylist(data[0].id)
+      return response.status(204).json(data);
+    } else {
+      return response.status(404).json({"error": "Record not found"});
+    }
+  }
+  catch(error) {
+    return response.status(500).json({"error": "Request could not be handled"});
+  }
+});
 //
 // router.post('/', async function (request, response) {
 //   try {
