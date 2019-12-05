c0ad9d62a6640d46b1557c9eec84deb02776b1d1
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-05 14:01:19 -0700

    Create playlists route and add playlist route to app.js
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    
    Co-Authored-By: Mack Halliday <Mackenzie.Halliday@gmail.com>

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const configuration = require('./knexfile')[environment];
 
 var indexRouter = require('./routes/index');
 var favoritesRouter = require('./routes/api/v1/favorites');
+var favoritesRouter = require('./routes/api/v1/playlists');
 
 var app = express();
 
@@ -20,5 +21,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/api/v1/favorites', favoritesRouter);
+app.use('/api/v1/playlists', playlistsRouter);
 
 module.exports = app;
