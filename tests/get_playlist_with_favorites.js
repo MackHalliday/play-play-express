e6ec9a37d1b3ae57e5ecde2bcf959b5a9225b052
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-10 17:59:34 -0700

    Testing for get favorites by playlist, need sad path
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <jkjgsherwood@gmail.com>

---
+++ tests/get_playlist_with_favorites.js
@@ -0,0 +1,76 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+describe('test GET playlist with favorites path', () => {
+  beforeEach(async () => {
+    await database.raw('truncate table favorites_playlist cascade');
+    await database.raw('truncate table playlists cascade');
+    await database.raw('truncate table favorites cascade');
+
+    await database('favorites').insert([
+      {id: 1, title: 'Track 1', artistName: 'Artist', genre: 'Genre', rating: 50},
+      {id: 2, title: 'Track 2', artistName: 'Artist', genre: 'Genre', rating: 100},
+      {id: 3, title: 'Track 3', artistName: 'Artist', genre: 'Genre', rating: 90},
+      {id: 4, title: 'Track 4', artistName: 'Artist', genre: 'Genre', rating: 2}
+    ]);
+
+    await database('playlists').insert([
+      {id: 1, title: 'Playlist 1'},
+      {id: 2, title: 'Playlist 2'},
+      {id: 3, title: 'Playlist 3'},
+      {id: 4, title: 'Playlist 4'}
+    ]);
+
+    await database('favorites_playlist').insert([
+      {id: 1, favorites_id: 1, playlists_id: 1},
+      {id: 1, favorites_id: 2, playlists_id: 1},
+      {id: 1, favorites_id: 3, playlists_id: 1},
+      {id: 2, favorites_id: 2, playlists_id: 2},
+      {id: 3, favorites_id: 3, playlists_id: 3},
+    ]);
+  });
+
+  afterEach(async () => {
+    await database.raw('truncate table favorites_playlist cascade');
+    await database.raw('truncate table playlists cascade');
+    await database.raw('truncate table favorites cascade');
+  });
+
+  describe('test get all favorites for playlist', () => {
+    it('happy path', async () => {
+      const response = await request(app)
+        .get("/api/v1/playlists/1/favorites");
+
+      expect(response.statusCode).toBe(201);
+      expect(response.body.length).toBe(1);
+
+      expect(response.body[0]).toHaveProperty('id');
+      expect(response.body[0]).toHaveProperty('title');
+      expect(response.body[0]).toHaveProperty('songCount');
+      expect(response.body[0]).toHaveProperty('songAvgRating');
+      expect(response.body[0]).toHaveProperty('favorites');
+      expect(response.body[0]).toHaveProperty('updated_at');
+      expect(response.body[0]).toHaveProperty('created_at');
+
+      expect(response.body[0].title).toBe('Playlist 1');
+      expect(response.body[0].songCount).toBe(4);
+      expect(response.body[0].songAvgRating).toBe(80);
+      expect(response.body[0].favorites.length).toBe(4);
+      expect(response.body[0].favorites[0].title).toBe("Track 2");
+      expect(response.body[0].favorites[0].artistName).toBe("Artist");
+      expect(response.body[0].favorites[0].rating).toBe(100);
+    });
+
+    // it('sad path', async () => {
+    //   const response = await request(app)
+    //     .delete("/api/v1/playlists/1/favorites/10000");
+    //
+    //   expect(response.statusCode).toBe(404);
+    // });
+  });
+});
