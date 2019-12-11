b1f993df8018d93c79ec9f0fb2d12de39772ea3d
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-10 17:00:35 -0700

    Update constructor with async function that returns songCount
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- models/playlist_object.js
+++ models/playlist_object.js
@@ -3,22 +3,21 @@ const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
 class PlaylistObject {
-  constructor(playlist){
-    this.getSongCount(playlist)
-    this.playlist = playlist;
-    this.id = playlist[0].id;
-    this.title= playlist[0].title;
-    this.songCount = this.getSongCount(playlist);
-    this.updated_at= playlist[0].updated_at;
-    this.created_at= playlist[0].created_at;
-  }
+    constructor(playlist) {
+      return (async () => {
+        this.id = await playlist[0].id;
+        this.title= await playlist[0].title;
+        this.songCount = await this.getSongCount(playlist);
+        this.updated_at= await playlist[0].updated_at;
+        this.created_at= await playlist[0].created_at;
+        return this;
+      })();
+    }
 
   async getSongCount(playlist){
     let favorites_count = await database('favorites_playlist').where('playlists_id', playlist[0].id).select();
-    console.log(favorites_count.length)
     return favorites_count.length
   }
-
 }
 
 module.exports = PlaylistObject
