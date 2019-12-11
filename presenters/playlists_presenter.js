86b085d4c1877c334c624c7b158b7a8d4674b20c
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-11 14:40:40 -0700

    Method to get all playlists_favorites records
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <jkjgsherwood@gmail.com>

--- presenters/playlists_presenter.js
+++ presenters/playlists_presenter.js
@@ -2,6 +2,12 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../knexfile')[environment];
 const database = require('knex')(configuration);
 
+const Favorites = require('../models/favorites.js');
+const favorites = new Favorites();
+
+const Playlists = require('../models/playlists.js');
+const playlists = new Playlists();
+
 const PlaylistObject = require('../models/playlist_object.js');
 
 class PlaylistsPresenter {
@@ -13,6 +19,16 @@ class PlaylistsPresenter {
     return await Promise.all(allPlaylists.map(async (playlist) => {
        return this.createPlaylistObject(playlist)
     }));
+  }
+
+  async validPlaylistFavorite(favoriteId, playlistId){
+    let favorite = await favorites.findFavorite(favoriteId)
+    let playlist = await playlists.findPlaylist(playlistId)
+    if (favorite[0] && playlist[0]){
+      return {favorite: favorite[0].title, playlist: playlist[0].title}
+    } else {
+      undefined
+    }
   }
 }
 
