a2bd7c84e8cf3cb58e688b0416844b7b91ae15d3
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-10 17:59:34 -0700

    Can find average tracks rating, can find all tracks
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- models/playlist_object.js
+++ models/playlist_object.js
@@ -8,11 +8,30 @@ class PlaylistObject {
         this.id = await playlist[0].id;
         this.title= await playlist[0].title;
         this.songCount = await this.getSongCount(playlist);
+        this.songAvgRating = await this.getSongAverageRating(playlist)
+        this.favorites = await this.getFavorites(playlist)
         this.updated_at= await playlist[0].updated_at;
         this.created_at= await playlist[0].created_at;
         return this;
       })();
     }
+
+  async getFavorites(playlist){
+    let favorites = await database('favorites')
+    .join('favorites_playlist', 'favorites.id', '=', 'favorites_playlist.favorites_id')
+    .where('playlists_id', playlist[0].id)
+    .select()
+    return favorites
+  }
+
+  async getSongAverageRating(playlist){
+   let favorites = await database('favorites')
+   .join('favorites_playlist', 'favorites.id', '=', 'favorites_playlist.favorites_id')
+   .where('playlists_id', playlist[0].id)
+   .avg('rating')
+   .first()
+   return parseFloat(favorites.avg).toFixed(2)
+  }
 
   async getSongCount(playlist){
     let favorites_count = await database('favorites_playlist').where('playlists_id', playlist[0].id).select();
