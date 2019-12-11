d0484d6ea9412c61ea32e70b3c7fc5f8cff9013a
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-11 12:23:11 -0700

    Update testing with new attributes in GET playlist endpoint
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <jkjgsherwood@gmail.com>

--- tests/all_playlists.spec.js
+++ tests/all_playlists.spec.js
@@ -8,19 +8,37 @@ const database = require('knex')(configuration);
 
 describe('test playlists path', () => {
   beforeEach(async () => {
+    await database.raw('truncate table favorites_playlist cascade');
     await database.raw('truncate table playlists cascade');
+    await database.raw('truncate table favorites cascade');
+
+    await database('favorites').insert([
+      {id: 1, title: 'Track 1', artistName: 'Artist', genre: 'Genre', rating: 50},
+      {id: 2, title: 'Track 2', artistName: 'Artist', genre: 'Genre', rating: 100},
+      {id: 3, title: 'Track 3', artistName: 'Artist', genre: 'Genre', rating: 90},
+      {id: 4, title: 'Track 4', artistName: 'Artist', genre: 'Genre', rating: 2}
+    ]);
 
     await database('playlists').insert([
-      {title: '80s Rock', id: 1},
-      {title: 'Chill Mix', id: 2},
-      {title: 'Jazz Playlist', id: 3},
-      {title: 'EDM Playlist', id: 4}
+      {id: 1, title: 'Playlist 1'},
+      {id: 2, title: 'Playlist 2'},
+      {id: 3, title: 'Playlist 3'},
+      {id: 4, title: 'Playlist 4'}
     ]);
-    console.log(database('playlists').first())
+
+    await database('favorites_playlist').insert([
+      {id: 1, favorites_id: 1, playlists_id: 1},
+      {id: 2, favorites_id: 2, playlists_id: 1},
+      {id: 3, favorites_id: 3, playlists_id: 1},
+      {id: 4, favorites_id: 2, playlists_id: 2},
+      {id: 5, favorites_id: 3, playlists_id: 3},
+    ]);
   });
 
-  afterEach(() => {
-    database.raw('truncate table playlists cascade');
+  afterEach(async () => {
+    await database.raw('truncate table favorites_playlist cascade');
+    await database.raw('truncate table playlists cascade');
+    await database.raw('truncate table favorites cascade');
   });
 
   describe('test playlists GET', () => {
@@ -33,10 +51,17 @@ describe('test playlists path', () => {
 
       expect(response.body[0]).toHaveProperty('id');
       expect(response.body[0]).toHaveProperty('title');
+      expect(response.body[0]).toHaveProperty('songCount');
+      expect(response.body[0]).toHaveProperty('songAvgRating');
+      expect(response.body[0]).toHaveProperty('favorites');
       expect(response.body[0]).toHaveProperty('updated_at');
       expect(response.body[0]).toHaveProperty('created_at');
 
-      expect(response.body[0].title).toBe('80s Rock');
+      expect(response.body[0].title).toBe('Playlist 1');
+      expect(response.body[0].songCount).toBe(3);
+      expect(response.body[0].songAvgRating).toBe(80);
+      expect(response.body[0].favorites.length).toBe(3);
+      expect(response.body[0].favorites[0].title).toBe('Track 1');
     });
   });
 });
