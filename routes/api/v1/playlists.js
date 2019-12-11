c83e3fc068897b7f0992f93265837fcd0412abfa
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-10 21:35:49 -0700

    Change conditional for for sad path for get playlist with favorites to return error message when playlist cannot be found
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    
    Co-Authored-By: Mack Halliday <Mackenzie.Halliday@gmail.com>

--- routes/api/v1/playlists.js
+++ routes/api/v1/playlists.js
@@ -108,7 +108,7 @@ router.post('/', async function (request, response) {
 router.get('/:id/favorites', async function (request, response) {
   try {
     let playlist = await playlists.findPlaylist(request.params.id)
-    if (playlist){
+    if (playlist.length === 1){
       let playlistObject = await new PlaylistObject(playlist[0])
       return response.status(201).json(playlistObject);
     } else {
