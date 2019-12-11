64b75b96eed62fbe3a51c188f6f7b3410a1487d7
Joshua Sherwood <jkjgsherwood@gmail.com>
2019-12-11 00:33:34 -0700

    Add happy and sad path testing for post favorite to playlist in post favorite to playlist spec
    Co-authored-by: Mack Halliday 
    <16658577+MackHalliday@users.noreply.github.com>
    
    Co-Authored-By: Mack Halliday <Mackenzie.Halliday@gmail.com>

---
+++ tests/post_favorite_to_playlist.spec.js
@@ -0,0 +1,72 @@
+var shell = require('shelljs');
+var request = require("supertest");
+var app = require('../app');
+
+const environment = process.env.NODE_ENV || 'test';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+
+describe('test POST favorite to playlist', () => {
+  beforeEach(async () => {
+    await database.raw('truncate table favorites_playlist cascade');
+    await database.raw('truncate table playlists cascade');
+    await database.raw('truncate table favorites cascade');
+
+    await database('favorites').insert([
+      {id: 1, title: 'The Chain', artistName: 'Fleetwood Mac', genre: 'Rock', rating: 52},
+      {id: 2, title: 'Truth Hurts', artistName: 'Lizzo', genre: 'Pop', rating: 40}
+    ]);
+
+    await database('playlists').insert([
+      {id: 1, title: '80s Rock'},
+      {id: 2, title: 'Chill Mix'}
+    ]);
+  });
+
+  afterEach(() => {
+    database.raw('truncate table favorites_playlist cascade');
+    database.raw('truncate table playlists cascade');
+    database.raw('truncate table favorites cascade');
+  });
+
+  it('user can add a favorite to a playlist', async () => {
+    let response = await request(app)
+      .post("/api/v1/playlists/2/favorites/2");
+
+    // expect(await database('favorites').where('id', 2)).toBe(201);
+
+    expect(response.statusCode).toBe(201);
+
+    expect(response.body).toEqual({"Success": "Truth Hurts has been added to Chill Mix!"});
+
+    let lastCreatedFavoritePlaylist = await database('favorites_playlist').orderBy('created_at', 'desc').first()
+
+    expect(lastCreatedFavoritePlaylist.favorites_id).toBe(2);
+    expect(lastCreatedFavoritePlaylist.playlists_id).toBe(2);
+
+  });
+
+  it('user cannot add a favorite to a playlist without a valid favorite or playlist id', async () => {
+    let response = await request(app)
+      .post("/api/v1/playlists/1/favorites/2000");
+
+    expect(response.statusCode).toBe(400);
+
+    expect(response.body).toEqual({"error": "Please enter a valid playlist and/or favorite id"});
+
+  });
+
+  it('user cannot add a favorite to a playlist twice', async () => {
+    await request(app)
+      .post("/api/v1/playlists/2/favorites/2");
+
+    let response2 = await request(app)
+      .post("/api/v1/playlists/2/favorites/2");
+
+    expect(response2.statusCode).toBe(400);
+
+    expect(response2.body).toEqual({"error": "The song has already been added to the playlist"});
+
+  });
+});
