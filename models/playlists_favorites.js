26520b6c4e0441bc424837e5d4bb1f7682d6e71e
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-11 14:44:02 -0700

    Create class to handle db calls for playlists_favorite table, method to get all playlists_favorite records
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <jkjgsherwood@gmail.com>

---
+++ models/playlists_favorites.js
@@ -0,0 +1,16 @@
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('../knexfile')[environment];
+const database = require('knex')(configuration);
+
+class PlaylistsFavorites {
+  constructor(){
+  }
+
+  async allPlaylistsFavorites(favoriteId, playlistId) {
+    return database('favorites_playlist')
+    .where('favorites_id', favoriteId)
+    .where('playlists_id', playlistId)
+  }
+}
+
+module.exports = PlaylistsFavorites
