8decbd40334c686b3e7741d7d8bfdb86459604a4
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-10 17:59:34 -0700

    Moved functions to playlist class
    Co-authored-by: Joshua Sherwood 
    <49769068+joshsherwood1@users.noreply.github.com>
    
    Co-Authored-By: null <jkjgsherwood@gmail.com>

--- models/playlist_object.js
+++ models/playlist_object.js
@@ -1,42 +1,18 @@
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('../knexfile')[environment];
-const database = require('knex')(configuration);
+const Playlists = require('../models/playlists.js');
+const playlists = new Playlists();
 
 class PlaylistObject {
-    constructor(playlist) {
-      return (async () => {
-        this.id = await playlist[0].id;
-        this.title= await playlist[0].title;
-        this.songCount = await this.getSongCount(playlist);
-        this.songAvgRating = await this.getSongAverageRating(playlist)
-        this.favorites = await this.getFavorites(playlist)
-        this.updated_at= await playlist[0].updated_at;
-        this.created_at= await playlist[0].created_at;
-        return this;
-      })();
-    }
-
-  async getFavorites(playlist){
-    let favorites = await database('favorites')
-    .join('favorites_playlist', 'favorites.id', '=', 'favorites_playlist.favorites_id')
-    .where('playlists_id', playlist[0].id)
-    .columns('favorites_id as id','title', 'artistName', 'genre', 'rating')
-    .select()
-    return favorites
-  }
-
-  async getSongAverageRating(playlist){
-   let avgRating = await database('favorites')
-   .join('favorites_playlist', 'favorites.id', '=', 'favorites_playlist.favorites_id')
-   .where('playlists_id', playlist[0].id)
-   .avg('rating')
-   .first()
-   return parseFloat(avgRating.avg).toFixed(2)
-  }
-
-  async getSongCount(playlist){
-    let favorites_count = await database('favorites_playlist').where('playlists_id', playlist[0].id).select();
-    return favorites_count.length
+  constructor(playlist) {
+    return (async () => {
+      this.id = await playlist[0].id;
+      this.title= await playlist[0].title;
+      this.songCount = await playlists.getSongCountByPlaylist(playlist[0].id);
+      this.songAvgRating = await playlists.getSongAverageRating(playlist[0].id)
+      this.favorites = await playlists.getFavoritesByPlaylist(playlist[0].id)
+      this.updated_at= await playlist[0].updated_at;
+      this.created_at= await playlist[0].created_at;
+      return this;
+    })();
   }
 }
 
