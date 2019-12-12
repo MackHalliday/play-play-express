395889da230e7c1d35ccb8c40865173c0eb51795
MackHalliday <mackenzie.halliday@gmail.com>
2019-12-11 17:40:34 -0700

    If statement logic updated to if artist present then call API with artist title in params
    Co-authored-by: Joshua Sherwood <49769068+joshsherwood1@users.noreply.github.com>

--- services/musix_match_service.js
+++ services/musix_match_service.js
@@ -5,11 +5,11 @@ class MusixMatchService {
     this.baseUrl = "http://api.musixmatch.com/ws/1.1"
   }
   async getTrackSearch(songName, artist){
-    if (artist == undefined) {
-      let response = await fetch(`${this.baseUrl}/track.search?q_track=${songName}&s_track_rating=desc&apikey=${process.env.MUSIX_MATCH_API_KEY}`)
+    if (artist) {
+      let response = await fetch(`${this.baseUrl}/track.search?q_track=${songName}&q_artist=${artist}&s_track_rating=desc&apikey=${process.env.MUSIX_MATCH_API_KEY}`)
       return response.json();
     } else {
-      let response = await fetch(`${this.baseUrl}/track.search?q_track=${songName}&q_artist=${artist}&s_track_rating=desc&apikey=${process.env.MUSIX_MATCH_API_KEY}`)
+      let response = await fetch(`${this.baseUrl}/track.search?q_track=${songName}&s_track_rating=desc&apikey=${process.env.MUSIX_MATCH_API_KEY}`)
       return response.json();
     };
 
